4732bb94b0ea73e2a662a375178058cace3d562f
David Gracia <david.gracia@code-sherpas.rocks>
2022-05-26 19:17:44 +0200

    feature 3. test

--- lib/src/test/kotlin/sample/SpaceshipTest.kt
+++ lib/src/test/kotlin/sample/SpaceshipTest.kt
@@ -1,6 +1,17 @@
 package sample
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 
 class SpaceshipTest {
+
+    @Test
+    fun `spaceship A shoots at spaceship B, B health is decremented by 1`(){
+        val spaceshipA = Spaceship(5,"A")
+        val spaceshipB = Spaceship(5, "B")
+        spaceshipA.shootAt(spaceshipB)
+        assertThat(spaceshipB.getHealth()).isEqualTo(4)
+    }
 
 }
